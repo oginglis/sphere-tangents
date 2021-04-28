35b784c6b5b253daa76c25f969cb2a431e6c50cc
Oliver Inglis <ollie@theinglistwins.com>
2021-04-28 18:34:50 +0100

    Lens"

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ThreeApp msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import ThreeApp from './components/ThreeApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ThreeApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 0 0 0;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="button">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  title: 'Sphere Tangents',
+  name: 'Button',
+  props: {
+
+  },
+  data() {
+    return {
+    }
+  },
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+
+</style>
